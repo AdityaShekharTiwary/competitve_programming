3ec9fd780c999a306785773c194850c12cf50033
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-08-27 12:38:54 +0530

    Add files via upload

---
+++ palindrome_string.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    int l,i;
+    cin >> s;
+    l = s.size() - 1;
+   // for (int i=0;i<=l;i++)
+
+        if(s[i] == s[l-i])
+
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+
+
+
+    return 0;
+}
