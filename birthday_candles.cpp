2bb07e717391f1f6d4c2b6cb5a78770c753a2512
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-08-29 10:52:30 +0530

    Add files via upload

---
+++ birthday_candles.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int n , arr[100000] ;
+    int count=0 ;
+    int max=arr[0];
+    cin >> n;
+    for (int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+        if (arr[i] > max);
+            max= arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==max)
+            count++;
+    }
+    cout << count << endl;
+        return 0;
+}
+
