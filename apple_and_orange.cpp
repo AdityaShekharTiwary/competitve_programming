d58af08124e0226dbfffe34d9665796424e9a382
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-08-31 23:39:22 +0530

    Add files via upload

---
+++ apple_and_orange.cpp
@@ -0,0 +1,19 @@
+#include<iostream
+#include<cstdio>
+using namespace std;
+int main(){
+    int s,t,a,b,m,n,countm=0,countn=0,temp;
+    scanf("%d %d",&s,&t);
+    scanf("%d %d",&a,&b);
+    scanf("%d %d",&m,&n);
+    while(m--){
+        scanf("%d",&temp);
+        if(temp>=s-a && temp<=t-a)countm++;
+    }
+    while(n--){
+        scanf("%d",&temp);
+        if(temp>=s-b && temp<=t-b)countn++;
+    }
+    printf("%d\n%d",countm,countn);
+    return 0;
+}
