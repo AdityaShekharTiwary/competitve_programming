1a479cde8a802d7e8e88813c1e2283aaee28b9b7
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-01 11:52:45 +0530

    Add files via upload

---
+++ duration.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a,b,c;
+    cin >> n;
+    int sh , sm ,eh ,em;
+    for(int i=0;i<n;i++)
+    {
+        cin >> sh >> sm >> eh >> em;
+
+        a= sh* 60 + sm;    //converting hour in min
+        b= eh* 60 + em;   //converting hour in min
+
+        c= b-a;           // total working hour
+
+        cout << c/60 << " " << c%60 << endl;
+
+    }
+    return 0;
+}
