b6f8b5c306a5087e53fe2b647748b24feecfe301
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-01 11:31:52 +0530

    Add files via upload

---
+++ char_sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int weight=0;
+    for(int i=0;i<s.size();i++)
+    {
+        weight+=s[i]-96;
+    }
+    cout << weight << endl;
+    return 0;
+}
