69e7b318db7f0806df945eabfe317f9d9d849332
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-01 22:55:15 +0530

    Add files via upload

---
+++ kangaroo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x1,v1,x2,v2,pos1=x1,pos2=x2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    if((x1 - x2) % (v2 - v1) == 0)
+        cout << "YES" << endl;
+    else
+        cout<< "NO" << endl;
+    return 0;
+}
