7206a0eaba70b87868fc93ff0844f245150b3e97
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-03 12:30:52 +0530

    Add files via upload

---
+++ safe_bots.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int sa,sb,i,k,j;
+        string s;
+        cin >> s;
+        cin >> sa >> sb;
+        for(i=0;i<s.size();i++)
+        {
+            if(s[i]== 'A')
+               k = i;
+            else if(s[i]== 'B')
+               j = i;
+        }
+        if((j-i)%(sa+sb)==0){
+            cout<<"unsafe"<<endl;
+        }
+        else cout<<"safe"<<endl;
+
+    }
+
+    return 0;
+}
