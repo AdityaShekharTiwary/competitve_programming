9722476c1b6e28695d53efcfc8f2a5cee2769092
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-07 12:01:24 +0530

    Add files via upload
    Given two strings of equal length, you have to tell whether they both strings are identical.
    
    Two strings S1 and S2 are said to be identical, if any of the permutation of string S1 is equal to the string S2. See Sample explanation for more details.
    
    Input :
    
    First line, contains an intger 'T' denoting no. of test cases.
    Each test consists of a single line, containing two space separated strings S1 and S2 of equal length.
    Output:
    
    For each test case, if any of the permutation of string S1 is equal to the string S2 print YES else print NO.
    Constraints:
    
    1<= T <=100
    1<= |S1| = |S2| <= 10^5
    String is made up of lower case letters only.
    Note : Use Hashing Concept Only . Try to do it in O(string length) .

---
+++ two_strings.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+
+    int t;
+
+    cin>>t;
+
+    while(t--)
+
+    {
+
+        string s1,s2;
+
+        cin>>s1>>s2;
+
+        sort(s1.begin(),s1.end());
+
+        sort(s2.begin(),s2.end());
+
+        if(s1==s2)
+
+        {
+
+            cout<<"YES"<<endl;
+
+        }
+
+        else
+
+        {
+
+            cout<<"NO"<<endl;
+
+        }
+
+    }
+
+}
