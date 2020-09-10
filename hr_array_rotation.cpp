7fa2713022e9be05590affaf477d3ce99777d77d
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-10 19:16:40 +0530

    HackerRank Array Rotation
    Ques. Link :-    https://www.hackerrank.com/challenges/array-left-rotation/problem

---
+++ hr_array_rotation.cpp
@@ -0,0 +1,19 @@
+// C++ program to rotate an array by
+// d elements
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n,d;
+    cin>>n;
+    int a[n];
+    cin>>d;
+    for(int i=0;i<n;cin>>a[i],i++);
+    d=d%n;
+    for(int i=d;i<n;i++)
+        cout<<a[i]<<" ";
+
+    for(int i=0;i<d;i++)
+         cout<<a[i]<<" ";
+    return 0;
+}
